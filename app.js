89bdc768dad858e0a55d82744969106aeb26385a
Kenny <kennethichoi@gmail.com>
2018-10-29 22:23:32 -0700

    Completed css variable with input manipulation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('connected!')
+// Event Listeners
+let inputs = document.querySelectorAll('.controls input');
+inputs.forEach(function(input) {
+    input.addEventListener('change', changeElements);
+    input.addEventListener('mousemove', changeElements);
+});
+
+// Functions
+function changeElements() {
+    let suffix = this.dataset.sizing || '';
+    
+    console.log(this.value);
+    console.log(suffix);
+
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
